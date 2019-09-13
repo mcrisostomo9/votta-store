2e23f4e0547031de8d9b4a39f568f733ea1aa51a
Mark Crisostomo <mcrisostomo9@gmail.com>
2019-09-13 16:13:12 -0700

    refactored product listings

--- src/components/ProductListing/ProductListing.js
+++ src/components/ProductListing/ProductListing.js
@@ -1,52 +1,20 @@
 import React from "react"
 import styled from "@emotion/styled"
 import ProductListingItem from "./ProductListingItem"
-import { graphql, useStaticQuery } from "gatsby"
-
-const PRODUCTS_LISTING_QUERY = graphql`
-  query ProductsListingQuery {
-    allShopifyProduct {
-      edges {
-        node {
-          id
-          title
-          handle
-          images {
-            localFile {
-              childImageSharp {
-                fixed(width: 200) {
-                  ...GatsbyImageSharpFixed_withWebp_tracedSVG
-                }
-              }
-            }
-          }
-          description
-          descriptionHtml
-          variants {
-            sku
-            id
-            title
-            price
-          }
-        }
-      }
-    }
-  }
-`
 
 const ProductWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   article {
-    width: 100%;
+    width: 25%;
   }
 `
 
-const ProductListing = () => {
-  const { allShopifyProduct } = useStaticQuery(PRODUCTS_LISTING_QUERY)
+const ProductListing = ({ products }) => {
   return (
     <ProductWrapper>
-      {allShopifyProduct.edges.map(({ node: product }) => {
+      {products.map(product => {
         return <ProductListingItem product={product} key={product.id} />
       })}
     </ProductWrapper>
