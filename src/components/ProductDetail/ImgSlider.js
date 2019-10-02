eb17c2226cf2d35eb9a88c3f87217aa895ae138a
Mark Crisostomo <mcrisostomo9@gmail.com>
2019-10-02 15:22:01 -0700

    fixed image to fit within container

--- src/components/ProductDetail/ImgSlider.js
+++ src/components/ProductDetail/ImgSlider.js
@@ -3,9 +3,26 @@ import Img from "gatsby-image"
 import styled from "@emotion/styled"
 import "react-id-swiper/lib/styles/css/swiper.css"
 import Swiper from "react-id-swiper"
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md"
+import { colors } from "../../utils/styles"
 
 const Container = styled.div`
   overflow: hidden;
+
+  .swiper-container {
+    height: 100%;
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    background: transparent;
+  }
+  .swiper-pagination-bullet-active {
+    background: ${colors.darkGrey};
+  }
+`
+
+const ImgContainer = styled.div`
+  height: 100%;
 `
 
 const ImgSlider = ({ images }) => {
@@ -19,15 +36,25 @@ const ImgSlider = ({ images }) => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    renderPrevButton: () => (
+      <MdKeyboardArrowLeft className="swiper-button-prev" />
+    ),
+    renderNextButton: () => (
+      <MdKeyboardArrowRight className="swiper-button-next" />
+    ),
   }
   return (
     <Container>
       <Swiper {...params}>
         {images.map((image, i) => {
           return (
-            <div>
-              <Img fluid={image.localFile.childImageSharp.fluid} key={i} />
-            </div>
+            <ImgContainer key={i}>
+              <Img
+                fluid={image.localFile.childImageSharp.fluid}
+                style={{ height: "100%" }}
+                imgStyle={{ objectFit: "contain" }}
+              />
+            </ImgContainer>
           )
         })}
       </Swiper>
