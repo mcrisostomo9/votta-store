fd77ef6fda6bc1e2dfdbce9afafb483602c716d7
Mark Crisostomo <mcrisostomo9@gmail.com>
2020-10-16 14:31:19 -0700

    upped to 50 products

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require("path")
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
   const product = await graphql(`
     query PagesQuery {
-      allShopifyProduct(limit: 10) {
+      allShopifyProduct(limit: 50) {
         edges {
           node {
             id
